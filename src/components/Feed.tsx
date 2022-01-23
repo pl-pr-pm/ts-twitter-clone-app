cf7986b67598d58ab18484c8fdc16b978b5fc346
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-23 18:22:39 +0900

    投稿一覧表示機能

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -3,6 +3,7 @@ import { db } from "../firebase";
 import TweetInput from "./TweetInput";
 import styles from "./Feed.module.css";
 import { UnsubscribeTwoTone } from "@material-ui/icons";
+import Post from "./Post";
 
 const Feed: React.FC = () => {
   const [posts, setPosts] = useState([
@@ -32,7 +33,15 @@ const Feed: React.FC = () => {
     <div className={styles.feed}>
       <TweetInput />
       {posts.map((post) => (
-        <h3>{post.id}</h3>
+        <Post
+          key={post.id}
+          postId={post.id}
+          avatar={post.avatar}
+          image={post.image}
+          text={post.text}
+          timestamp={post.timestamp}
+          username={post.username}
+        />
       ))}
     </div>
   );
